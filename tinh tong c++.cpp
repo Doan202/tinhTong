7963e070cd77df91016c5c6a9610cc5ac98ddadb
doan202 <doan202mta@gmail.com>
2023-07-01 23:23:01 +0700

    cap nhat lan thu 1

---
+++ tinh tong c++.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int n;
+	cout<<"nhap gia tri n"<<endl;
+	cin>>n;
+	int sum=0;
+	for(int i=0; i<=n; i++) {
+		sum+=i;
+	}
+	cout<<"tong cua n la "<<sum;
+	return 0;
+}
